3ce043b01dd0de648d7a2e6203b6ea8efe149ddc
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-23 16:20:10 +0530

    added thumbnails in feed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from "react";
+import { abbreviateNumber } from "js-abbreviation-number";
+import { Link } from "react-router-dom";
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
-const VideoCard = () => {
+// import VideoLength from "../shared/videoLength";
+
+const VideoCard = ({video}) => {
   return (
-    <div>VideoCard</div>
+    <Link to={`/video/${video?.videoId}`}>
+      <div className="flex flex-col mb-8">
+        <div className="relative h-48 md:h-40 md:rounded-xl overflow-hidden ">
+          <img 
+            className="h-full w-full object-cover"
+            src={video?.thumbnails?.[0]?.url}
+            alt=""
+          />
+        </div>
+      </div>
+    </Link>
   )
 }
 
