5b90b9acf68eb7a2d6ef7fd8d6ca76f2927cd333
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-23 17:11:30 +0530

    added all the remaining data like views subscriber verified badge

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -16,9 +16,9 @@ const Feed = () => {
             <LeftNav />
             <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
-                    {!loading &&
-                        searchResults.map((item) => {
-                            if (item.type !== "video") return false;
+                    {!loading && searchResults &&
+                        searchResults?.map((item) => {
+                            if (item?.type !== "video") return false;
                             return (
                                 <VideoCard
                                     key={item?.video?.videoId}
