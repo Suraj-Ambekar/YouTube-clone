6e9aa87b207b6c14b7fbb398b3ec9e70e1b1ae98
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-23 15:47:03 +0530

    solved bug

--- src/context/contextApi.js
+++ src/context/contextApi.js
@@ -1,41 +1,40 @@
-import React, {createContext, useState, useEffect} from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 import { fetchDataFromApi } from "../utils/api";
-
 export const Context = createContext();
 
 export const AppContext = (props) => {
     const [loading, setLoading] = useState(false);
-    const [searchResults, setSearchResults] = useState(false);
-    const [selectCategories, setSelectCategories] = useState("New");
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("New");
     const [mobileMenu, setMobileMenu] = useState(false);
 
+    useEffect(() => {
+        fetchSelectedCategoryData(selectedCategory);
+    }, [selectedCategory]);
 
-useEffect(() => {
-    fetchSelectedCategoryData(selectCategories);
-}, [selectCategories]);
+    const fetchSelectedCategoryData = (query) => {
+        setLoading(true);
+        fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
+            console.log(contents);
+            setSearchResults(contents);
+            setLoading(false);
+        });
+    };
 
-const fetchSelectedCategoryData = (query) =>{
-    setLoading(true);
-    fetchDataFromApi(`search/?q=${query}`).then(({contents})=> {
-        console.log(contents);
-        setSearchResults(contents);
-        setLoading(false);
-    })
-}
-
-return (
-    <Context.Provider value={{
-        loading,
-        setLoading,
-        searchResults,
-        setSearchResults,
-        selectCategories,
-        setSelectCategories,
-        mobileMenu,
-        setMobileMenu,
-    }}>
-        {props.children}
-    </Context.Provider>
-)
-}
+    return (
+        <Context.Provider
+            value={{
+                loading,
+                setLoading,
+                searchResults,
+                selectedCategory,
+                setSelectedCategory,
+                mobileMenu,
+                setMobileMenu,
+            }}
+        >
+            {props.children}
+        </Context.Provider>
+    );
+};
