dee7857a36f7bbf7db296852c8ae1051d43cb5e6
Suraj Ambekar <ambekarsuraj03@gmail.com>
2023-02-24 15:06:58 +0530

    added youtube video, channel logo, name , verified badge, and subscriber count

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -55,6 +55,31 @@ const VideoDetails = () => {
             <div className="text-white font-bold text-sm md:text-xl mt-4 line-clamp-2">
               {video?.title}
             </div>
+            <div className="flex justify-between flex-col md:flex-row mt-4">
+              <div className="flex">
+                <div className="flex items-start">
+                  <div className="flex h-11 w-11 rounded-full overflow-hidden">
+                    <img 
+                      className="h-full w-full object-cover"
+                      src={video?.author?.avatar[0]?.url}
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col ml-3">
+                  <div className="text-white text-md font-semibold flex items-center">
+                    {video.author.title}
+                    {video?.author?.badges[0]?.type ===
+                        "VERIFIED_CHANNEL" && (
+                          <BsFillCheckCircleFill className="text-white/[0.5] text-[12px] ml-1" />
+                    )}
+                  </div>
+                  <div className="text-white/[0.7] text-sm">
+                    {video?.author?.stats?.subscribersText}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
